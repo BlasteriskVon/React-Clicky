8c6e8d0e22219fd37d2057568a4adb4850b34c65
Andrew V <red1000@rocketmail.com>
2019-09-05 21:45:50 -0400

    Updates

---
+++ precache-manifest.017852fc027664196d6803b30fee6afb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6928da0a877741fd65d528657ed109",
+    "url": "/React-Clicky/index.html"
+  },
+  {
+    "revision": "fa6ec17bc58636740eec",
+    "url": "/React-Clicky/static/js/2.7b2ae981.chunk.js"
+  },
+  {
+    "revision": "026dcd1d4dc3537e10d9",
+    "url": "/React-Clicky/static/js/main.f990f5e6.chunk.js"
+  },
+  {
+    "revision": "4cf41a11eb6159852e5b",
+    "url": "/React-Clicky/static/js/runtime~main.f49c148b.js"
+  }
+]);
